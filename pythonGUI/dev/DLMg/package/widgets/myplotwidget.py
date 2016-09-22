433bc0aaba922a3caaef73348e21b339a41f681f
Richie Slocum <richie.slocum@cormorantanalytics.com>
2016-09-22 14:12:53 -0700

    disable right click on plot

--- pythonGUI/dev/DLMg/package/widgets/myplotwidget.py
+++ pythonGUI/dev/DLMg/package/widgets/myplotwidget.py
@@ -5,7 +5,7 @@ import pyqtgraph as pg
 
 class MyPlotWidget(pg.PlotWidget):
     def __init__(self, nplots):
-        super(MyPlotWidget, self).__init__()
+        super(MyPlotWidget, self).__init__(enableMenu=False)
         self.plotItem.setLabel('left','Acceleration','g')
         self.plotItem.setLabel('bottom', 'Time', 's')
         self.plotItem.setTitle('DLM Accelerations')
